4e5cbbc04e3d6b8073385dc59ccf2292fba10303
Renato Eduardo Figueiredo Alves <renatofigueiredoalves@gmail.com>
2022-05-08 11:49:17 -0300

    add library photosphor-react

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,5 @@
-interface ButtonProps {
-  text?: string;
-}
+import { Widget } from "./components/Widget";
 
-function Button(props: ButtonProps) {
- return <button className="bg-violet-500 p-2">{props.text ?? 'Default'}</button>
-}
-
-function App() {
-  return (
-    <div>
-      <Button text="Enviar" />
-      <Button text="OK" />
-      <Button />
-    </div>
-  )
-}
-
-export default App
+export function App() {
+  return <Widget />
+} 
---
+++ src/components/Widget.tsx
@@ -0,0 +1,5 @@
+import { ChatTeardropDots } from "phosphor-react";
+
+export function Widget() {
+  return <h1><ChatTeardropDots size="200" /></h1>
+}
